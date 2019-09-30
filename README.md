0ff65957f69c98bdbcefcfb0df81b45dc90a99f8
Pauline Cairns <37514155+pac9@users.noreply.github.com>
2019-09-30 16:28:29 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+To recreate this project, please:
+
+- Update the config.php file to your own database settings.
+
+- Set up a database.
+
+- To create and populate the database tables:
+   Copy and paste code from setup.sql and 
+   then in browser run /resourcing/sql/user_password.php 
+   (eg copy and paste http://localhost:81/resourcing/sql/user_password.php into browser updating the path as required)
+
+If having difficulties with the <script> elements, may have to do 
+alt + enter to download the libraries
+   
+As the 'uploads' directory was empty when the last push was done to github, it is not appearing in the repository.  If doing a pull, will also need to set up an 'uploads' directory.
---
+++ delete/delete_module_task.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pauline
+ * Date: 17/03/2019
+ * Purpose: takes input from full_list.php form and posts it to the database
+ */
+session_start();
+include("../config.php");
+
+if (!IsSet($_SESSION["userID"]))		//user variable must exist in session to stay here
+    header("Location: login.php");	//if not, go back to login page
+$username=$_SESSION["userID"];		//get user name into variable $username
+
+//Processing input from  full_list.php
+$id=$_POST["id"];
+
+//Insert data to database
+$sql = "DELETE FROM module_task WHERE id='$id'";
+
+//check post to DB successful and redirect back to full_list page
+if(mysqli_query($db, $sql)){
+
+}else {echo "Error: ". $sql . "<br". mysqli_error($db);
+}
+
+header("location: ../full_list.php");
+$db->close();
+
+?>
